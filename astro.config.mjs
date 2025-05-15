e78d3d66de8c5c0ea737ac2ca27c5a3e020afb1a
Michael Will <michael.will@rumble.com>
2025-05-14 21:40:45 -0600

    install tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   base: '/integrated-response-group/',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
