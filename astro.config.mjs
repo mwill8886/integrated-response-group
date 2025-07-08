143f932e02730a3d2ca6af0228e0f760dbff75c3
michael will <13811701+mwill8886@users.noreply.github.com>
2025-07-07 19:57:04 -0600

    add prod workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 
+const isProd = process.env.SITE_ENV === 'prod';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://dev.integratedresponsegroup.com',
+  site: isProd
+    ? 'https://integratedresponsegroup.com'
+    : 'https://dev.integratedresponsegroup.com',
   base: '/',
-  // base: '/integrated-response-group/',
+  outDir: 'dist',
   vite: {
     plugins: [tailwindcss(), svgr()],
   },
