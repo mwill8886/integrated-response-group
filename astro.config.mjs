043cd7700d74f9e0edb2af103f7eab76d0e74ac2
michael will <13811701+mwill8886@users.noreply.github.com>
2025-07-07 21:38:30 -0600

    check env

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 
+console.log("🔧 SITE_ENV:", process.env.SITE_ENV);
+
 const isProd = process.env.SITE_ENV === 'prod';
 
 // https://astro.build/config
